bee0cdc71ed76734b8269f6af1eadbbf26aa0a3f
sangariej <sangariej@berea.edu>
2019-11-23 18:09:44 -0500

    prework_hello_world

---
+++ app/src/main/java/codepath/com/MainActivity.java
@@ -0,0 +1,24 @@
+package codepath.com;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 ((TextView) findViewById(R.id.textView)).setTextColor(
+                         getResources().getColor(R.color.colorPrimary));
+             }
+         });
+    }
+
+}
